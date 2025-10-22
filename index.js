2f0d435aee33ad9d6019b68d7438706cb972bf1f
tutashkhia-pixel <davidakirtava@gmail.com>
2025-10-22 00:00:43 -0400

    Initialize Express server with CAdd basic Express serverORS and dotenv

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('InboxRescue API is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
