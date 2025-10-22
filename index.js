f835cbb588efd6173c89acb7d7131adaad19866e
tutashkhia-pixel <davidakirtava@gmail.com>
2025-10-22 00:09:11 -0400

    Register auth and summarization routes in Express server

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const authRoutes = require('./routes/auth');
+const summarizeRoutes = require('./routes/summarize');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// Register route handlers
+app.use('/auth', authRoutes);
+app.use('/summarize', summarizeRoutes);
 
 app.get('/', (req, res) => {
   res.send('InboxRescue API is running');
